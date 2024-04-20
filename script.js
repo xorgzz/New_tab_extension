56fe688c3b8b0e6d0a34faee09f4dc6d814eea5c
xorgzz <118397053+xorgzz@users.noreply.github.com>
2024-04-20 23:25:08 +0200

    Delete script.js
    make place for new version

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('bar').onkeypress = function(e){
-	if (!e) e = window.event;
-	if (e.keyCode == '13' && document.getElementById('bar').value.trim() != ''){
-		window.location="https://duckduckgo.com/?q=" + encodeURIComponent(this.value);
-		return false;
-	}
-}
