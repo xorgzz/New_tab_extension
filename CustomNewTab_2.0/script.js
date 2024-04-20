c4511a5041cc16524722b259ee77704b097d3a8e
xorgzz <118397053+xorgzz@users.noreply.github.com>
2024-04-20 23:24:45 +0200

    Delete CustomNewTab_2.0 directory
    make place for new version

--- CustomNewTab_2.0/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('bar').onkeypress = function(e){
-	if (!e) e = window.event;
-	if (e.keyCode == '13' && document.getElementById('bar').value.trim() != ''){
-		// https://duckduckgo.com/?q=
-		window.location="https://www.google.com/search?q=" + encodeURIComponent(this.value);
-		return false;
-	}
-}
